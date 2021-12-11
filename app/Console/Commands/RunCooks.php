2db1ce28f3589b5ee93485d3e4e26f12298debe3
Russell Galpin <russ@lrhosting.net>
2021-12-11 16:44:33 +0000

    Set up scheduler

--- app/Console/Commands/RunCooks.php
+++ app/Console/Commands/RunCooks.php
@@ -3,14 +3,13 @@
 namespace App\Console\Commands;
 
 use App\Models\Cook;
-use App\Models\Guru;
 use App\Models\Probe;
 use App\Models\Reading;
 use App\Services\Guru\CyberQ;
 use Carbon\Carbon;
 use Illuminate\Console\Command;
 
-class GetTemperatures extends Command
+class RunCooks extends Command
 {
     /**
      * The name and signature of the console command.
